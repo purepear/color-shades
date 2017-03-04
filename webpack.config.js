dacd0fb279e82f3e500f783d02ca54549173bb86
purepear <purepear@gmail.com>
2017-03-04 16:45:21 +0200

    Turn on sourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
+      sourceMap: true,
       compress: {
         warnings: false
       }
